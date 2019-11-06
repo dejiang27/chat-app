937bc368ce174709f778ab864aba80bc8c6a578c
Dejiang Chen <dejiang27@gmail.com>
2019-11-05 18:03:15 -0600

    chat-app working version 1.0

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ChatNavBarComponent } from './chat-nav-bar/chat-nav-bar.component';
 import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ChatThreadComponent } from './chat-thread/chat-thread.component';
 
 import { UsersService } from './user/users.service';
 import { ThreadsService } from './thread/threads.service';
 import { MessagesService } from './message/messages.service';
+import { FromNowPipe } from './pipes/from-now.pipe';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatNavBarComponent,
     ChatThreadsComponent,
-    ChatWindowComponent
+    ChatWindowComponent,
+    ChatPageComponent,
+    ChatMessageComponent,
+    ChatThreadComponent,
+    FromNowPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     MessagesService,
--- chat-app/src/app/chat-nav-bar/chat-nav-bar.component.ts
+++ chat-app/src/app/chat-nav-bar/chat-nav-bar.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
+import * as _ from 'lodash';
+import { ThreadsService } from './../thread/threads.service';
+import { MessagesService } from './../message/messages.service';
+import { Thread } from './../thread/thread.model';
+import { Message } from './../message/message.model';
+import { combineLatest } from 'rxjs/operators';
 @Component({
   selector: 'app-chat-nav-bar',
   templateUrl: './chat-nav-bar.component.html',
@@ -7,9 +13,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatNavBarComponent implements OnInit {
 
-  constructor() { }
+  unreadMessagesCount: number;
 
-  ngOnInit() {
+  constructor(public messagesService: MessagesService,
+              public threadsService: ThreadsService) {
+  }
+
+  ngOnInit(): void {
+    this.messagesService.messages.pipe(
+      combineLatest(
+        this.threadsService.currentThread,
+        (messages: Message[], currentThread: Thread) =>
+          [currentThread, messages] )
+    )
+      .subscribe(([currentThread, messages]: [Thread, Message[]]) => {
+        this.unreadMessagesCount =
+          _.reduce(
+            messages,
+            (sum: number, m: Message) => {
+              const messageIsInCurrentThread: boolean = m.thread &&
+                currentThread &&
+                (currentThread.id === m.thread.id);
+              // note: in a "real" app you should also exclude
+              // messages that were authored by the current user b/c they've
+              // already been "read"
+              if (m && !m.isRead && !messageIsInCurrentThread) {
+                sum = sum + 1;
+              }
+              return sum;
+            },
+            0);
+      });
   }
 
 }
